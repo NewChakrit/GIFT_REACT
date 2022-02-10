81f27b5d89ec8d94139e86283338b423abeeebaa
NewChakrit <imnew108@gmail.com>
2022-02-10 01:47:09 -0800

    'save'

--- src/components/layouts/mainlayout/footer/Footer.jsx
+++ src/components/layouts/mainlayout/footer/Footer.jsx
@@ -1,24 +1,33 @@
-import './Footer.css';
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
-    return (
-        <>
-            <div className="main-footer">
-                <div className="logo home">
-                    <i className="bi bi-gift"></i>
-                </div>
-                <div className="logo map">
-                    <i className="bi bi-geo-alt"></i>
-                </div>
-                <div className="logo message">
-                    <i className="bi bi-chat-heart"></i>
-                </div>
-                <div className="logo profile">
-                    <i className="bi bi-person"></i>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="main-footer">
+        <Link to="/">
+          <div className="logo home">
+            <i className="bi bi-gift"></i>
+          </div>
+        </Link>
+        <Link to="/">
+          <div className="logo map">
+            <i className="bi bi-geo-alt"></i>
+          </div>
+        </Link>
+        <Link to="messenger">
+          <div className="logo message">
+            <i className="bi bi-chat-heart"></i>
+          </div>
+        </Link>
+        <Link to="profile">
+          <div className="logo profile">
+            <i className="bi bi-person"></i>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
