fa1574526bb385d890ccd851c068334f9a76edbd
AofPatiphan <patiphan.ma@hotmail.com>
2022-02-11 13:28:42 +0700

    save

--- src/pages/chat/footerChat/FooterChat.jsx
+++ src/pages/chat/footerChat/FooterChat.jsx
@@ -1,18 +1,18 @@
-import "./FooterChat.css";
+import './FooterChat.css';
 
 function FooterChat() {
-  return (
-    <div className="main-footer footer-chat">
-      <input
-        type="text"
-        class="form-control"
-        placeholder="Aa"
-        aria-label="Aa"
-        aria-describedby="addon-wrapping"
-      />
-      <i class="bi bi-arrow-up-square"></i>
-    </div>
-  );
+    return (
+        <div className="main-footer footer-chat">
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Aa"
+                aria-label="Aa"
+                aria-describedby="addon-wrapping"
+            />
+            <i className="bi bi-arrow-up-square"></i>
+        </div>
+    );
 }
 
 export default FooterChat;
--- src/pages/editprofile/EditprofileHeader.jsx
+++ src/pages/editprofile/EditprofileHeader.jsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 
 function EditprofileHeader() {
-  return (
-    <div className="main-header fixed-top">
-      <div className="header-left">
-        <i class="bi bi-arrow-left"></i>
-      </div>
-      <div>
-        <h2 className="header-name">Edit Profile</h2>
-      </div>
-      <div className="header-right">
-        <i class="bi bi-save"> Save</i>
-      </div>
-    </div>
-  );
+    return (
+        <div className="main-header fixed-top">
+            <div className="header-left">
+                <i className="bi bi-arrow-left"></i>
+            </div>
+            <div>
+                <h2 className="header-name">Edit Profile</h2>
+            </div>
+            <div className="header-right">
+                <i className="bi bi-save"> Save</i>
+            </div>
+        </div>
+    );
 }
 
 export default EditprofileHeader;
