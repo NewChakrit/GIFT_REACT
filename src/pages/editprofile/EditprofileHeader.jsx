e6a12412d598cfe1b2f08c026c7222afa97f2e80
Thail3 <tanai.saeng@gmail.com>
2022-02-11 10:54:31 +0700

    create bone modal

---
+++ src/pages/editprofile/EditprofileHeader.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function EditprofileHeader() {
+  return (
+    <div className="main-header fixed-top">
+      <div className="header-left">
+        <i class="bi bi-arrow-left"></i>
+      </div>
+      <div>
+        <h2 className="header-name">Edit Profile</h2>
+      </div>
+      <div className="header-right">
+        <i class="bi bi-save"> Save</i>
+      </div>
+    </div>
+  );
+}
+
+export default EditprofileHeader;
