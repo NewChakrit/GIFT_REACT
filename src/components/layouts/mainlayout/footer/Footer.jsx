859fd6e073ac30d566b41b0ee5519bb1adf2b96c
AofPatiphan <patiphan.ma@hotmail.com>
2022-02-11 09:53:10 +0700

    save

--- src/components/layouts/mainlayout/footer/Footer.jsx
+++ src/components/layouts/mainlayout/footer/Footer.jsx
@@ -1,33 +1,39 @@
-import "./Footer.css";
-import { Link } from "react-router-dom";
+import './Footer.css';
+import { Link, useLocation } from 'react-router-dom';
 
 function Footer() {
-  return (
-    <>
-      <div className="main-footer">
-        <Link to="/">
-          <div className="logo home">
-            <i className="bi bi-gift"></i>
-          </div>
-        </Link>
-        <Link to="/">
-          <div className="logo map">
-            <i className="bi bi-geo-alt"></i>
-          </div>
-        </Link>
-        <Link to="messenger">
-          <div className="logo message">
-            <i className="bi bi-chat-heart"></i>
-          </div>
-        </Link>
-        <Link to="profile">
-          <div className="logo profile">
-            <i className="bi bi-person"></i>
-          </div>
-        </Link>
-      </div>
-    </>
-  );
+    const location = useLocation();
+    console.log(location.pathname);
+
+    if (location.pathname.includes('/chat')) {
+        return <></>;
+    }
+    return (
+        <>
+            <div className="main-footer">
+                <Link to="/">
+                    <div className="logo home">
+                        <i className="bi bi-gift"></i>
+                    </div>
+                </Link>
+                <Link to="/">
+                    <div className="logo map">
+                        <i className="bi bi-geo-alt"></i>
+                    </div>
+                </Link>
+                <Link to="messenger">
+                    <div className="logo message">
+                        <i className="bi bi-chat-heart"></i>
+                    </div>
+                </Link>
+                <Link to="profile">
+                    <div className="logo profile">
+                        <i className="bi bi-person"></i>
+                    </div>
+                </Link>
+            </div>
+        </>
+    );
 }
 
 export default Footer;
