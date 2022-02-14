7fb94da1f6968a14e17c83dbb14b4cf36778554b
NewChakrit <imnew108@gmail.com>
2022-02-14 03:09:28 -0800

    'save'

--- src/components/post/postcontent/PostContent.jsx
+++ src/components/post/postcontent/PostContent.jsx
@@ -1,54 +1,56 @@
-import React from 'react';
-import './postcontent.css';
+import React from "react";
+import "./postcontent.css";
 
 function PostContent({ item }) {
-    return (
-        <div
-            className="modal fade"
-            id={`modal${item.id}`}
-            tabIndex="-1"
-            aria-labelledby={`${item.id}ModalLabel`}
-            aria-hidden="true"
-        >
-            <div className="modal-dialog modal-dialog-centered">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id={`${item.id}ModalLabel`}>
-                            Modal title
-                        </h5>
-                        <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                        ></button>
-                    </div>
-                    <div className="modal-body">
-                        <div>{item.caption}</div>
-                        <div>
-                            <img
-                                className="postPicture"
-                                src={item.pictureUrl}
-                                alt=""
-                            />
-                        </div>
-                    </div>
-                    <div className="modal-footer">
-                        <button
-                            type="button"
-                            className="btn btn-secondary"
-                            data-bs-dismiss="modal"
-                        >
-                            Close
-                        </button>
-                        <button type="button" className="btn btn-primary">
-                            Save changes
-                        </button>
-                    </div>
-                </div>
+  return (
+    <div
+      className="modal fade"
+      id={`modal${item.id}`}
+      tabIndex="-1"
+      aria-labelledby={`${item.id}ModalLabel`}
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <div className="left-menu">
+              <button type="button" className="btn btn-light">
+                Edit
+              </button>
+
+              <button type="button" className="btn btn-light">
+                Delete
+              </button>
             </div>
+
+            <div className="right-menu">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+          </div>
+          <div className="modal-body">
+            <div>{item.caption}</div>
+            <div>
+              <img className="postPicture" src={item.pictureUrl} alt="" />
+            </div>
+          </div>
+          <div className="modalFooter">
+            <button
+              className="btn btn-footer"
+              style={{ height: "50px", width: "60px" }}
+            >
+              <i className="bi bi-heart-fill heartIcon" />
+            </button>
+            <div className="like-amount">200</div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default PostContent;
