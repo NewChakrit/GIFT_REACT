d14a36814e5458d54ed72fc3110d330da0646fa4
NewChakrit <imnew108@gmail.com>
2022-02-20 06:57:58 -0800

    'eiei'

--- src/components/auths/login/Login.jsx
+++ src/components/auths/login/Login.jsx
@@ -1,70 +1,75 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../../contexts/AuthContext';
-import './login.css';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthContext";
+import "./login.css";
 
 function Login() {
-    const { email, setEmail, password, setPassword, handleSubmitLogin } =
-        useContext(AuthContext);
+  const { email, setEmail, password, setPassword, handleSubmitLogin } =
+    useContext(AuthContext);
 
-    return (
-        <div className="login">
-            <div className="logo">
-                <img
-                    src="https://res.cloudinary.com/dbtlgaii3/image/upload/v1644303690/Gift/Gift_v1_xq6xdr.jpg"
-                    alt="Logo"
-                />
-            </div>
-            <div className="loginForm">
-                <form className="pt-3 p-5" onSubmit={handleSubmitLogin}>
-                    <div className="mb-3 pb-4">
-                        <input
-                            type="email"
-                            className="form-control inputLogin"
-                            id="emailInput"
-                            placeholder="E-mail"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <i className="bi bi-envelope placeHolderEmail"></i>
-                    </div>
-                    <div className="mb-3 ">
-                        <input
-                            type="password"
-                            className="form-control inputLogin"
-                            id="passwordInput"
-                            placeholder="Password "
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-check pb-5">
-                        <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id="flexCheckChecked"
-                            style={{ border: 'none' }}
-                        />
-                        <i className="bi bi-eye-slash placeHolderPassword"></i>
-                        {/* <i className="bi bi-eye placeHolderPassword"></i> */}
-                        <label
-                            className="form-check-label rememberme"
-                            htmlFor="flexCheckChecked"
-                        >
-                            Remember Me
-                        </label>
-                    </div>
-                    <button type="submit" className="btn loginButton">
-                        Login
-                    </button>
-                    <div className="signUpLink">
-                        <Link to={'/register'}>Sign up for Gift</Link>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="login">
+      <div className="logo">
+        <img
+          src="https://res.cloudinary.com/do58tgs2e/image/upload/v1645345853/gift_plmqmv.png"
+          alt="Logo"
+        />
+        <img
+          className="sparkle"
+          //   src="https://media.giphy.com/media/WNwLx0skSQOPCgbxNL/giphy.gif"
+          src="https://media.giphy.com/media/Y4hQj1h0eVbE3rIInW/giphy.gif"
+        />
+      </div>
+      <div className="loginForm">
+        <form className="pt-3 p-5" onSubmit={handleSubmitLogin}>
+          <div className="mb-3 pb-4">
+            <input
+              type="email"
+              className="form-control inputLogin"
+              id="emailInput"
+              placeholder="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <i className="bi bi-envelope placeHolderEmail"></i>
+          </div>
+          <div className="mb-3 ">
+            <input
+              type="password"
+              className="form-control inputLogin"
+              id="passwordInput"
+              placeholder="Password "
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="form-check pb-5">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckChecked"
+              style={{ border: "none" }}
+            />
+            <i className="bi bi-eye-slash placeHolderPassword"></i>
+            {/* <i className="bi bi-eye placeHolderPassword"></i> */}
+            <label
+              className="form-check-label rememberme"
+              htmlFor="flexCheckChecked"
+            >
+              Remember Me
+            </label>
+          </div>
+          <button type="submit" className="btn loginButton">
+            Login
+          </button>
+          <div className="signUpLink">
+            <Link to={"/register"}>Sign up for Gift</Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -4,17 +4,20 @@ import { motion } from "framer-motion";
 const item = {
   hidden: {
     opacity: 0,
+    scale: 4,
     y: 0,
   },
   show: {
     opacity: 1,
+    scale: 1,
     transition: {
       ease: [0.6, 0.01, -0.5, 0.95],
-      duration: 1.6,
+      duration: 2,
     },
   },
   exit: {
     opacity: 0,
+    scale: 3,
     transition: {
       ease: "easeInOut",
       duration: 1,
@@ -28,15 +31,20 @@ function Loader({ setLoginLoading }) {
       <motion.div
         className="loader-com"
         variants={item}
-        initial="hidden"
         animate="show"
         exit="exit"
         onAnimationComplete={() => setLoginLoading(false)}
       >
-        <motion.div className="transition-image" variants={item}>
+        <motion.div
+          className="transition-image"
+          variants={item}
+          initial="hidden"
+          animate="show"
+          exit="exit"
+        >
           <img
             className="loader-img"
-            src="https://media.giphy.com/media/xT0GqFhyNd0Wmfo6sM/giphy.gif"
+            src="https://media.giphy.com/media/5bsOIT6O4igIc1EnUZ/giphy.gif"
           />
         </motion.div>
       </motion.div>
