e93bcbd9c86c889fe213f1d3856dbe779a4d4b7f
AofPatiphan <patiphan.ma@hotmail.com>
2022-02-22 09:29:32 +0700

    save

--- src/config/env.jsx
+++ src/config/env.jsx
@@ -1,4 +1,4 @@
-// const API_ENDPOINT_URL = 'http://localhost:5555';
+// const API_ENDPOINT_URL = 'http://localhost:8888';
 const API_ENDPOINT_URL = 'https://soodpung-gift-app.herokuapp.com';
 
 export { API_ENDPOINT_URL };
